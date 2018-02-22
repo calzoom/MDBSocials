50e469189ce974977700f9f45a863b462a880e97
Japjot Singh <japjot@airbears2-10-142-38-200.airbears2.1918.berkeley.edu>
2018-02-21 20:54:10 -0800

    Start Page

--- app/src/main/java/com/example/japjot/mdbsocials/MainActivity.java
+++ app/src/main/java/com/example/japjot/mdbsocials/MainActivity.java
@@ -1,7 +1,16 @@
 package com.example.japjot.mdbsocials;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            
+
     }
+
 }
